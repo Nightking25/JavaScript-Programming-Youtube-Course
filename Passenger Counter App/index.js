8d7f1e0eb538aa3968c099c93b923e542390f034
Nightking25 <Sthanirdesh25@gmail.com>
2022-08-08 18:58:20 -0400

    Final Passenger Counter App

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,5 +1,27 @@
-//document.getElementById('count-el').innerText = 6
+// document.getElementById("count-el").innerText = 5;
 
-// let count = 5;
-// count++;
-// console.log(count);
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+// let entries = 0;
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let entries = count + " - ";
+  saveEl.textContent += entries;
+  count = 0;
+  countEl.textContent = count;
+}
+
+
+// let welcomeEl = document.getElementById('welcome-el');
+// let name = "Nirdesh Shrestha";
+// let greeting = "Welcome Back! ";
+// let myGreeting = greeting + name;
+//
+// welcomeEl.innerText = myGreeting;
+//
+// welcomeEl.innerText += String.fromCodePoint(0x1F44B);
