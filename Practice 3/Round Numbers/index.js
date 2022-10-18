0ac49711c2e2a5d6dddfa363579ee1481e376b31
Nightking25 <Sthanirdesh25@gmail.com>
2022-10-17 20:57:50 -0400

    final code

--- Practice 3/Round Numbers/index.js
+++ Practice 3/Round Numbers/index.js
@@ -4,5 +4,4 @@
 
 const totalPrice = 420.69235632455
 const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${ totalPrice }`
-
+btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
